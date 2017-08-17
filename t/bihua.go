b94f331e3d47515e43caefc79e3a73090317a2db
zzWinD <zzwind@live.cn>
2017-08-17 09:05:05 +0800

    查询笔画的小工具done

--- t/bihua.go
+++ t/bihua.go
@@ -1,24 +1,38 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
 func main() {
 	// fmt.Println()
-	fetchBihua("龚")
+	for {
+		fmt.Println("美女，输入你要查询笔画的词语")
 
-	var mySlice = []byte{244, 244, 244, 244, 244, 244, 244, 244}
-	data := binary.BigEndian.Uint64(mySlice)
-	fmt.Println(data)
+		var name string
+
+		fmt.Scan(&name)
+
+		// name := "龚启舟姜旭园"
+		count := 0
+		for _, n := range name {
+			count += fetchBihua(string(n))
+		}
+		fmt.Println(name + "笔画总数是:" + strconv.Itoa(count))
+		fmt.Println()
+
+		// var mySlice = []byte{244, 244, 244, 244, 244, 244, 244, 244}
+		// data := binary.BigEndian.Uint64(mySlice)
+		// fmt.Println(data)
+	}
 
 }
 
-func fetchBihua(s string) {
+func fetchBihua(s string) int {
 
 	r, err := http.Get("http://www.baidu.com/s?wd=" + s + "%20笔画")
 	if err != nil {
@@ -29,11 +43,18 @@ func fetchBihua(s string) {
 	re := regexp.MustCompile(`<div class="op_exactqa_detail_s_answer">(?:[.\s]*)<span>(\d*)</span>(?:[.\s]*)</div>`)
 	bb := re.FindSubmatch(b)
 
+	i, _ := strconv.Atoi(string(bb[1]))
+	return i
 
-	fmt.Printf("%s",bb[1])
-	fmt.Println(bb[1])
+	// fmt.Println(strconv.Atoi(string(bb[1])))
+	// ss, _ := utf8.DecodeRune(bb[1])
+	// fmt.Println(utf8.RuneLen(ss))
 
-	// fmt.Println(binary.BigEndian.Uint32(bb[1]))
+	// fmt.Printf("%s", bb[1])
+	// fmt.Println(bb[1])
 
-	fmt.Println("done")
+	// fmt.Println(binary.BigEndian.Uint16(bb[1]))
+	// fmt.Println(binary.LittleEndian.Uint16(bb[1]))
+
+	// fmt.Println("done")
 }
